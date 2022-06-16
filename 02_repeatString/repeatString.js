4c8326fef98a71f6d6265d19a6710fe92a28bc9c
Lokowar <lokwoar1@gmail.com>
2022-06-16 17:09:54 -0300

    Exercicio completo

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,22 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    if (num === 0){
+        string = ''
+    }
+    if (num === -1){
+        return string = "ERROR"
+    }
+    if (string === ''){
+        for (i = 2; i <= num; i++){
+            string += ""
+        }
+        return string
+    }
+    else{
+        for (i = 2; i <= num; i++){
+            string += "hey"
+        }
+    }
+    return string
 };
 
 // Do not edit below this line
