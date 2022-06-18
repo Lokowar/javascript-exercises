60228bef788a809a2bd0a9e3450d47f5277109a3
Lokowar <lokwoar1@gmail.com>
2022-06-18 16:23:36 -0300

    Exercicio feito

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
-
+const sumAll = function(x, y) {
+    if(typeof x === 'string' || typeof y === 'string'){
+        return "ERROR";
+    }
+    if(x < 0 || y < 0){
+        return "ERROR";
+    }
+    if (x < y){
+        for (i = 0; i <= y; i++){
+            x = x + i;
+        }
+        return x-1;
+    }
+    if (x > y){
+        for (i = 0; i <= x; i++){
+            y = y + i;
+        }
+        return y-1;
+    }else{
+        return "ERROR";
+    }
 };
 
 // Do not edit below this line
