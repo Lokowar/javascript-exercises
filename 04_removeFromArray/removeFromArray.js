1ea3c3155821b7d28cf6dcf9861fff3ca02509d0
Lokowar <lokwoar1@gmail.com>
2022-06-18 15:46:36 -0300

    Exercicio concluido

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,11 @@
-const removeFromArray = function() {
-
-};
+const removeFromArray = function(x, y1, y2, y3, y4) {
+    for (i = x.length; i >= 0; i--){
+        if (x[i] === y1 || x[i] === y2 || x[i] === y3 || x[i] === y4){
+            x.splice(i, 1);
+        }
+    }
+    return x;
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
