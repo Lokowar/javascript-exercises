10abbc290f75f1b5c69e7a674fa8ffecbd8c27e3
Lokowar <lokwoar1@gmail.com>
2022-07-03 16:13:11 -0300

    Exercicio feito

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,56 @@
-const add = function() {
-	
+const add = function(x, y) {
+  let result = x + y;
+
+	return result
 };
 
-const subtract = function() {
-	
+const subtract = function(x, y) {
+	let result = x - y;
+
+  return result;
 };
 
-const sum = function() {
-	
+const sum = function(x) {
+  let result = 0;
+  
+  for (i = 0; i < x.length; i++){
+    result = result + x[i];
+  }
+
+  return result;
 };
 
-const multiply = function() {
+const multiply = function(x) {
+  const valorInicial = 1;
 
+  const multiplieInicial = x.reduce(
+    (valorPrevio, valorAtual) => valorPrevio * valorAtual,
+    valorInicial
+  );
+
+  return multiplieInicial;
 };
 
-const power = function() {
-	
+const power = function(base, potencia) {
+  let result = base;
+
+	for (i = 1; i < potencia; i++){
+    result = result * base;
+  }
+
+  return result;
 };
 
-const factorial = function() {
-	
+const factorial = function(x) {
+  if(x===0){
+    return x = 1;
+  }
+
+  for (i = x; i >= 2; i--){
+    x = x * (i - 1);
+  }
+
+  return x;
 };
 
 // Do not edit below this line
--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,28 @@
-const palindromes = function () {
+const palindromes = function (x) {
+    for(i=0; i <= x.length; i++){
+        if(x[i] === "!"){
+            let copy;
 
+            copy = x.split("!").reverse().join("");
+
+            copy += "!";
+
+            return x === copy;
+        }
+        if(x[i] == " "){
+            let copy;
+            let copyOfCopy;
+
+            copy = x.replace(/\s|,/g, "");
+            copy = copy.toLowerCase();
+
+            copyOfCopy = copy.replace(".", "");
+
+            return copyOfCopy === copyOfCopy.split("").reverse().join("");
+        }
+    }
+
+    return x === x.split("").reverse().join("");
 };
 
 // Do not edit below this line
