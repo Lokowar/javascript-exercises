d7d2d65a21ac121b3568562b5b273d706a71b647
Lokowar <lokwoar1@gmail.com>
2022-07-03 17:40:57 -0300

    Exercicio feito

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,25 @@
-const fibonacci = function() {
+const fibonacci = function(tg) {
+    let x = 1;
+    let y = 1;
+    let z;
 
+    tg = parseInt(tg);
+
+    if (tg <= 2 && tg >= 0){
+        return 1;
+    }
+
+    if(tg < 0){
+        error = "OOPS";
+        return error;
+    }
+
+    for ( i = 0; i <= tg - 3; i++){
+        z = x + y;
+        y = x;
+        x = z;
+    }
+    return z;
 };
 
 // Do not edit below this line
