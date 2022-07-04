0ae038933368cc01157f08e79bea8d34a21dc491
Lokowar <lokwoar1@gmail.com>
2022-07-04 15:54:30 -0300

    Exercicio feito

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,14 @@
-const getTheTitles = function() {
+const getTheTitles = function(books) {
+    let book1;
+    let book2;
+    let res = [];
 
+    book1 = books.find(book => book.title === "Book");
+    book2 = books.find(book => book.title === "Book2");
+
+    res.push(book1.title, book2.title);
+
+    return res;
 };
 
 // Do not edit below this line
